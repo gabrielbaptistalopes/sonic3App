11ca4f635722005e8ab60c7a66e75c5a4672ea30
Gabriel Baptista Lopes <gabriel_b_lopes@hotmail.com>
2022-08-15 17:29:39 -0300

    Criada a model e a service dos Items

---
+++ sonicSega/sonicSega/Model+Services/Items+Model.swift
@@ -0,0 +1,18 @@
+//
+//  Items+Model.swift
+//  sonicSega
+//
+//  Created by Gabriel Lopes on 15/08/22.
+//
+
+import Foundation
+
+class Items {
+    let ItemPowerUp: String
+    let ItemImage: String
+    
+    init(ItemPowerUp: String, ItemImage: String) {
+        self.ItemImage = ItemImage
+        self.ItemPowerUp = ItemPowerUp
+    }
+}
---
+++ sonicSega/sonicSega/Model+Services/Items+Service.swift
@@ -0,0 +1,36 @@
+//
+//  Items+Service.swift
+//  sonicSega
+//
+//  Created by Gabriel Lopes on 15/08/22.
+//
+
+import Foundation
+import UIKit
+
+class ItemsService {
+    
+    let items: [Items] = [
+        
+    Items(ItemPowerUp: "Ring",
+          ItemImage: "big ring"),
+    
+    Items(ItemPowerUp: "Fire",
+          ItemImage: "fire"),
+    
+    Items(ItemPowerUp: "Life",
+          ItemImage: "life"),
+    
+    Items(ItemPowerUp: "Light",
+          ItemImage: "light"),
+    
+    Items(ItemPowerUp: "Speed",
+          ItemImage: "speed"),
+    
+    Items(ItemPowerUp: "Star",
+          ItemImage: "star"),
+    
+    Items(ItemPowerUp: "Water",
+          ItemImage: "water"),
+    ]
+}
