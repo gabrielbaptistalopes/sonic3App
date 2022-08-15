0f76cbcfab35bc72527511d17703969b8e73bd6f
Gabriel Baptista Lopes <gabriel_b_lopes@hotmail.com>
2022-08-15 17:19:49 -0300

    Criada a collectionview cell para items e adicionado os assets especificos

---
+++ sonicSega/sonicSega/ItemsCollectionViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  ItemsCollectionViewCell.swift
+//  sonicSega
+//
+//  Created by Gabriel Lopes on 15/08/22.
+//
+
+import UIKit
+
+class ItemsCollectionViewCell: UICollectionViewCell {
+    
+    
+    @IBOutlet weak var itemsImageView: UIImageView!
+    @IBOutlet weak var ItemsDescriptionLabel: UILabel!
+    
+}
--- sonicSega/sonicSega/Views/ItemsViewController.swift
+++ sonicSega/sonicSega/Views/ItemsViewController.swift
@@ -8,12 +8,33 @@
 import UIKit
 
 class ItemsViewController: UIViewController {
-
+    
+    
+    @IBOutlet weak var ItemsCollectionView: UICollectionView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        ItemsCollectionView.dataSource = self
+        ItemsCollectionView.delegate = self
 
        
     }
     
 
+}
+
+extension ItemsViewController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        <#code#>
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        <#code#>
+    }
+    
+    
+}
+    
+extension ItemsViewController: UICollectionViewDelegate {
+    
 }
